b7de089cd629102fc26b2717cd34899b2f84a637
Fruithailu <Fruithailu@yahoo.com>
2023-03-04 09:14:32 -0800

    0-main.c, main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
+
 #define MAIN_H
+
+#include <stdio.h>
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c,);
@@ -7,4 +10,4 @@ void print_reamining_days(int month, int day, int year);
 int convert_day(int month, int day);
 void print_times_table(int n);
 
-#endif
+#endif /* MAIN_H*/
